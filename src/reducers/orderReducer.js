0dfebaddc9f45dff8b302c3eefa703bf66bf6feb
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2019-05-16 14:56:34 -0300

    initial version

---
+++ src/reducers/orderReducer.js
@@ -0,0 +1,12 @@
+import { LOAD_ORDER } from "../actions/action_types";
+export default (state = { order: [] }, action) => {
+  switch(action.type) {
+    case LOAD_ORDER:
+      return { 
+        ...state,
+        order: action.order
+      }
+    default:
+      return state
+  }
+}
